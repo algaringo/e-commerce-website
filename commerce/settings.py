b4147f6344846d29666eb471c05f2280462fa8b3
Angelu Garingo <algaringo@gmail.com>
2020-09-11 09:21:40 +0800

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # Activate Django-Heroku.
